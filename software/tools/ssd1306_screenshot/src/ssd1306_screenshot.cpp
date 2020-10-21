33a202675cc4bc54482136a04916fb9ba8faf8b0
Yevgeniy Olexandrenko <yevgeniy.olexandrenko@gameloft.com>
2020-10-21 21:57:04 +0300

    Modified algorithm for screenshot generation

--- software/tools/ssd1306_screenshot/src/ssd1306_screenshot.cpp
+++ software/tools/ssd1306_screenshot/src/ssd1306_screenshot.cpp
@@ -18,15 +18,28 @@ unsigned char getNibble(char ch)
     return 0;
 }
 
+void fillBackground(Image& image, const Image::Pixel& color)
+{
+    for (int y = 0; y < image.GetH(); ++y)
+    {
+        for (int x = 0; x < image.GetW(); ++x)
+        {
+            image.SetPixel(x, y, color);
+        }
+    }
+}
+
 void placeByteOnImage(Image& image, unsigned char byte, int byteNumber)
 {
     int c = (byteNumber & 0x007F);
     int p = (byteNumber & 0x0380) >> 7;
 
+    const Image::Pixel pixelOn (0x00, 0xFF, 0xFF);
+    const Image::Pixel pixelOff(0x00, 0x00, 0x00);
+
     for (int b = 0; b < 8; ++b)
     {
-        Image::Pixel::Channel i = Image::Pixel::Channel((byte & 0x01) != 0 ? 0xFF : 0x00);
-        image.SetPixel(c, p << 3 | b, Image::Pixel(i, i, i));
+        image.SetPixel(16 + c, 16 + (p << 3 | b), (byte & 0x01) != 0 ? pixelOn : pixelOff);
         byte >>= 1;
     }
 }
@@ -36,9 +49,10 @@ int main()
     std::ifstream fileStream("screenshot.txt");
     std::string fileContent((std::istreambuf_iterator<char>(fileStream)), std::istreambuf_iterator<char>());
 
-    int byteNumber = 0;
-    Image result(128, 64, false);
+    Image result(160, 96, false);
+    fillBackground(result, Image::Pixel(0x40, 0x40, 0x40));
 
+    int byteNumber = 0;
     State state = State::SearchForDataBegin;
     for (int i = 0; i < fileContent.size(); ++i)
     {
